cf0debb560ef19521b09a924cbc16774f121554e
BrovkoRoman <brovkora@rambler.ru>
2024-06-04 16:03:49 +0400

    redis

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -8,6 +8,8 @@ import org.example.service.LinkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Random;
+
 import static org.example.service.LinkServiceImpl.shortLinksDomain;
 
 @RestController
@@ -18,17 +20,19 @@ public class MainController {
         this.linkService = linkService;
     }
     @PostMapping(value = "/create")
-    public String createShortLink(@RequestBody String longLink)
-            throws IncorrectLongLinkException, BadRepositoryFunctionCallException {
+    public String createShortLink(@RequestBody String longLink) {
         if(longLink == null || longLink.isBlank())
             throw new IncorrectLongLinkException();
 
         return linkService.getShortLink(longLink);
     }
 
-    @Autowired
-    private LinksRepository linksRepository;
-
+    @PostMapping(value = "/generate")
+    public void generateShortLinks(@RequestBody String prefix) {
+        for(int i = 0; i < 1000; i++) {
+            linkService.getShortLink(String.format("%s%s.com", prefix, i));
+        }
+    }
 
     @GetMapping("/{shortCode}")
     public String getLongLink(@PathVariable("shortCode") String shortCode)
--- src/main/java/org/example/service/DBServiceImpl.java
+++ src/main/java/org/example/service/DBServiceImpl.java
@@ -4,9 +4,11 @@ import org.example.dao.entity.LinksEntity;
 import org.example.dao.repository.LinksRepository;
 import org.example.exception.BadRepositoryFunctionCallException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 @Service
 public class DBServiceImpl implements DBService {
@@ -16,7 +18,9 @@ public class DBServiceImpl implements DBService {
     public DBServiceImpl(LinksRepository linksRepository) { this.linksRepository = linksRepository; }
 
     @Override
+    @Cacheable(value = "DBServiceImpl::getShortCodeFromDB", key = "#longLink")
     public String getShortCodeFromDB(String longLink) {
+        System.out.println("getShortCodeFromDB");
         List<String> shortCodes = linksRepository.findShortCodesByLongLink(longLink);
 
         if (!shortCodes.isEmpty()) {
@@ -27,7 +31,9 @@ public class DBServiceImpl implements DBService {
     }
 
     @Override
+    @Cacheable(value = "DBServiceImpl::getLongLinkFromDB", key = "#shortCode")
     public String getLongLinkFromDB(String shortCode) {
+        System.out.println("getLongLinkFromDB");
         List<String> longLinks = linksRepository.findLongLinksByShortCode(shortCode);
 
         if(!longLinks.isEmpty()) {
@@ -38,19 +44,28 @@ public class DBServiceImpl implements DBService {
     }
 
     @Override
+    @Cacheable(value = "DBServiceImpl::containsShortCode", key = "#shortCode")
     public boolean containsShortCode(String shortCode) {
+        System.out.println("containsShortCode");
         List<String> longLinks = linksRepository.findLongLinksByShortCode(shortCode);
         return !longLinks.isEmpty();
     }
 
     @Override
+    @Cacheable(value = "DBServiceImpl::containsLongLink", key = "#longLink")
     public boolean containsLongLink(String longLink) {
+        System.out.println("containsLongLink");
         List<String> shortCodes = linksRepository.findShortCodesByLongLink(longLink);
         return !shortCodes.isEmpty();
     }
 
     @Override
+    @Caching(evict = {
+            @CacheEvict(value = "DBServiceImpl::containsLongLink", key = "#longLink"),
+            @CacheEvict(value = "DBServiceImpl::containsShortCode", key = "#shortCode")
+    })
     public void addPairOfLinks(String longLink, String shortCode) {
+        System.out.println("addPairOfLinks");
         linksRepository.save(new LinksEntity(shortCode, longLink));
     }
 }
